0d3eb93975f6c1b431bc65c73737251761a953d0
Gleb Gare.g <84378158+iamnotapr@users.noreply.github.com>
2022-01-25 18:37:08 +0700

    Add files via upload

---
+++ JavaScript/cookie/cookie-time.js
@@ -0,0 +1,7 @@
+//COOKIE TIME
+//COOKIE TIME/DATE CUSTOMER
+function cookie ( cookie_name ){
+    let cookie_date = new Date ( );                 //DATE COOKIE
+    cookie_date.setTime( cookie_date.getTime() -1);
+    document.cookie = cookie_name += "=; expires=" + cookie_date.toGMTString();
+}
